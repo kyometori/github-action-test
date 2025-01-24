ca84f20e6fbec6870ad295b4cd6656fe4394a19d
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2025-01-24 23:54:40 +0800

    chore: update the code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  int arg1 = std::stoi(argv[1]);
-  int arg2 = std::stoi(argv[2]);
+  const int arg1 = std::stoi(argv[1]);
+  const int arg2 = std::stoi(argv[2]);
 
-  std::cout << arg1 + arg2 << std::endl;
+  std::cout << arg1 + arg2;
 
   return 0;
 }
